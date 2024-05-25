1fea7d30cb7fc0776fc47552f72e448fc671aa95
Deepaselvakumar <sdeepait0711@gmail.com>
2024-05-26 01:03:28 +0530

    created

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const router = express.Router();
+const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect('mongodb://localhost:27017/studentForm')
+.then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.log(err);
+});
+const studentSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+const student = mongoose.model('details', studentSchema);
+router.get('/signin', (req, res) => {
+    res.render('signin');
+});
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const newStudent = new student({ email, password });
+        await newStudent.save();
+        console.log('signed successfully:', newStudent);
+        res.redirect('/login');
+    } catch (error) {
+        console.error('Error saving user :', error.message);
+        res.status(500).send('Error saving  database');
+    }
+});
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      
+        const user = await student.findOne({ email, password });
+        if (student) {
+            console.log('User logged in:', student);
+            res.redirect('/stuform');
+        } else {
+            res.send('Invalid email or password');
+        }
+    } catch (error) {
+        console.log(' logging error:', error.message);
+        res.status(500).send('logging error');
+    }
+});
+router.get('/stuform', (req, res) => {
+    res.render('stuform');
+});
+app.use(router);
+app.listen(port, () => {
+    console.log(`The server is listening on port ${port}`);
+});
+
